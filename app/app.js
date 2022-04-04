12676edc94c522eac2439d19b4d402b7b7d57336
kimtaehyun <taehyun0121@naver.com>
2022-04-04 14:56:53 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -5,11 +5,12 @@ const express =require('express');
 const app = express();
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static('${__dirname}/src/public'))
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 app.use("/", home); // use -> 미들웨어 등록해주는 메서드
 
